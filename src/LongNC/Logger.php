222f0c8a8f02207632a02a4ffd2fe3b2b2f6a865
Long. Nguyen Chi <longnc@vng.com.vn>
2016-06-14 01:30:04 +0700

    Up

--- src/LongNC/Logger.php
+++ src/LongNC/Logger.php
@@ -3,26 +3,26 @@ namespace LongNC;
 
 class Logger {
 
-    public function info($data) {
+    public static function info($data) {
         $class = debug_backtrace()[1]['class'];
         $function = debug_backtrace()[1]['function'];
-        \Monolog\Logger::info("$class\t$function".$this->getLogMessage($data));
+        \Monolog\Logger::info("$class\t$function". self::getLogMessage($data));
     }
 
-    public function error($data) {
+    public static function error($data) {
         $class = debug_backtrace()[1]['class'];
         $function = debug_backtrace()[1]['function'];
-        \Monolog\Logger::error("$class\t$function".$this->getLogMessage($data));
+        \Monolog\Logger::error("$class\t$function". self::getLogMessage($data));
     }
     
-    public function exception(\Exception $exc, $bPrintStackTrace = false) {
+    public static function exception(\Exception $exc, $bPrintStackTrace = false) {
         \Monolog\Logger::error($exc->getMessage());
         if ($bPrintStackTrace == true) {
             \Monolog\Logger::error($exc->getTraceAsString());
         }
     }
 
-    private function getLogMessage($data) {
+    public static function getLogMessage($data) {
         if (is_array($data)) {
             $message = json_encode($data);
         } else {
